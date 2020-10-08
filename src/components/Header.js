65a00167782e55098eba9836a9198157155cf760
Raghuram <1993raghuram@gmail.com>
2020-10-08 20:42:54 +0530

    Add Home Page and Fetch Articles

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <header className="bg-gray-300 py-3  shadow-md">
+      <div className="container mx-auto">
+        <h2 className="font-bold text-2xl text-blue-800">Conduit</h2>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
